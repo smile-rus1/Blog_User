c3829644e6404420eab4eeb4df519047201a17d5
Gasak Ruslan <rusya.gasak@mail.ru>
2023-03-03 19:02:07 +0300

    добавлена модель Post

--- blogs_users/blog/models.py
+++ blogs_users/blog/models.py
@@ -2,4 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Заголовок")
+    content = models.TextField(verbose_name="Текст статьи")
+    date_create = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания", auto_created=True)
+    date_update = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Посты пользователей"
+        ordering = ["-date_update"]
+
