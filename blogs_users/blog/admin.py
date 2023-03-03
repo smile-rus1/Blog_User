c76f0c7b7545decde0deed4d157af8d63c36d4f6
Gasak Ruslan <rusya.gasak@mail.ru>
2023-03-03 19:03:20 +0300

    зарегестрирована модель Post

--- blogs_users/blog/admin.py
+++ blogs_users/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "title")
+    list_display_links = ("id", "title")
+    search_fields = ("title",)
+
+
+admin.site.register(Post, PostAdmin)
