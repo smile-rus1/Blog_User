d9f53352568dbef30a50952a3bac76b0fa808849
Gasak Ruslan <rusya.gasak@mail.ru>
2023-03-06 18:21:17 +0300

    добавлена ссылка на посты пользователя добавлен путь в urls.py к постам пользователя

--- blogs_users/blog/urls.py
+++ blogs_users/blog/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("logout/", logout_user, name="logout"),
     path("add_new_post/", add_new_post, name="add_new_post"),
     path("post/<int:post_id>/", show_post, name="post"),
+    path("my_post/", my_posts, name="my_post"),
 
 ]
